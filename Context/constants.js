58a629e2253aad30c616e72de0e364f6bcf00942
Hans394 <farhanfarhannnn81@gmail.com>
2026-06-14 23:15:28 +0700

    feat: update contract address, add Sepolia network configuration, and initialize mock IPFS data

--- Context/constants.js
+++ Context/constants.js
@@ -1,6 +1,6 @@
 
 import ChatAppABIData from "./ChatApp.json";
 
-export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const ChatAppAddress = "0x3142702C24Bd2bD742537396b55525e570d33F16";
 
 export const ChatAppABI = ChatAppABIData;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env.local" });
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,6 +10,11 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
     },
   },
 };
